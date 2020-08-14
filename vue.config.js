26be67c4cc4b94d7f5a1f27c6d029096d4801263
Denis Joel Torres Farro <djoe_torres@hotmail.com>
2020-08-13 22:43:29 -0500

    second commit

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
     transpileDependencies: ["vuetify"],
     devServer: {
         proxy: 'http://localhost:8080/',
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/lukuma/' : '/'
 };
