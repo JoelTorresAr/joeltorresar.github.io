5ec57910ead2d06d4a4fa7c9a034c4f910649955
Denis Joel Torres Farro <djoe_torres@hotmail.com>
2020-08-14 19:56:17 -0500

    six commit

---
+++ dist/precache-manifest.f6a6941da14365a0b92c236cc8f0c81a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e09a610bd546a8fec4c",
+    "url": "/lukuma/css/chunk-0bbb75aa.4d8b579d.css"
+  },
+  {
+    "revision": "aafbca77f9e4bbcc8b52",
+    "url": "/lukuma/css/chunk-4a4f8b80.1047fbb6.css"
+  },
+  {
+    "revision": "dae63f71acbef6b50955",
+    "url": "/lukuma/css/chunk-536dc408.1bc855a0.css"
+  },
+  {
+    "revision": "bfd9ef58b4c5fcc13c27",
+    "url": "/lukuma/css/chunk-56177c72.e5a04aaf.css"
+  },
+  {
+    "revision": "3f422e0bb0c7275e9010",
+    "url": "/lukuma/css/chunk-71367435.ec70e45f.css"
+  },
+  {
+    "revision": "c386ed0e83fc07cc95b7",
+    "url": "/lukuma/css/chunk-vendors.71549ec7.css"
+  },
+  {
+    "revision": "1fb5844b5cd96a208183c3a6b5c23e7d",
+    "url": "/lukuma/index.html"
+  },
+  {
+    "revision": "f9ce2b89d267372572e4",
+    "url": "/lukuma/js/app.167f0ce3.js"
+  },
+  {
+    "revision": "1e09a610bd546a8fec4c",
+    "url": "/lukuma/js/chunk-0bbb75aa.08ba84a3.js"
+  },
+  {
+    "revision": "aafbca77f9e4bbcc8b52",
+    "url": "/lukuma/js/chunk-4a4f8b80.b77f7f59.js"
+  },
+  {
+    "revision": "dae63f71acbef6b50955",
+    "url": "/lukuma/js/chunk-536dc408.36ce2ebf.js"
+  },
+  {
+    "revision": "bfd9ef58b4c5fcc13c27",
+    "url": "/lukuma/js/chunk-56177c72.e1856296.js"
+  },
+  {
+    "revision": "3f422e0bb0c7275e9010",
+    "url": "/lukuma/js/chunk-71367435.879ce44f.js"
+  },
+  {
+    "revision": "c386ed0e83fc07cc95b7",
+    "url": "/lukuma/js/chunk-vendors.3317522f.js"
+  },
+  {
+    "revision": "08032d4b718500bb25054f5ff9685c22",
+    "url": "/lukuma/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lukuma/robots.txt"
+  }
+]);
